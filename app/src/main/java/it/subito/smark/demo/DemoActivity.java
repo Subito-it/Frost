f6b177349088c92be5d028a8eb6dd6d2c57d8b6b
Savino Ordine <savino.ordine@subito.it>
2014-06-09 17:23:16 +0200

    clean code

--- app/src/main/java/it/subito/smark/demo/DemoActivity.java
+++ app/src/main/java/it/subito/smark/demo/DemoActivity.java
@@ -19,8 +19,6 @@ package it.subito.smark.demo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
@@ -76,25 +74,6 @@ public class DemoActivity extends Activity {
                 mSmarkTextView.setAutoSave(isChecked);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void buttonClick() {
